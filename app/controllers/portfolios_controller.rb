d7c7a29353d343cc3e03cde37bcc8a25e8334517
San <skmf201704@gmail.com>
2017-11-20 22:17:56 +0900

    Adding MVC

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,26 @@
 class PortfoliosController < ApplicationController
+  
   def index
     @portfolio_items = Portfolio.all
+  end
+
+  def new 
+    @portfolio_item = Portfolio.new
+  end
+
+    # POST /blogs
+  # POST /blogs.json
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your Portfolio was successfully created.' }
+        # format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        # format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
