b9195d897b8c0ec8c7f25cbcbb08b444ff1d6089
San <skmf201704@gmail.com>
2017-11-20 21:15:40 +0900

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+ Blog.create!(
+  title: "My Blog Post #{blog}",
+  body: "Sample body. Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body.Sample body."
+  )
+end
+
+puts "10 blog post created"
+
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 30
+    )
+end
+
+
+puts "5 skill created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "This is subtitle",
+    body: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+    )
+end
+puts "9 portfolio created"
